3b712d67ed85c9c6424f3ded74bdf1a5dfe8b66a
Vineet <vineet98love@gmail.com>
2021-08-12 16:20:46 +0530

    block-BJaahs

--- block-BJaahs/code/index.js
+++ block-BJaahs/code/index.js
@@ -1,45 +1,72 @@
 // Without Object
 
+// let title = 'Where is the capital of Jordan';
+// let options = ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'];
+// let correctAnswerIndex = 1;
+
+// function isAnswerCorrect(){
+//     return index === correctAnswerIndex;
+// }
+// function getCorrectAnswer(){
+//     return options[correctAnswerIndex];
+// }
+
+
 
 // - Organize using object
 
+// let data = {
+//     title : 'Where is the capital of Jordan',
+//     options : ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'],
+//     correctAnswerIndex : 1,
+    
+//     isAnswerCorrect(index){
+//         return index === data.correctAnswerIndex;
+//     },
+//     getCorrectAnswer(){
+//         return data.options[data.correctAnswerIndex];
+//     }
+// }
 
 // - Use a function to create object
 
-function  getData(title, array, correctAnswerIndex){
-    let array = [];
+// function getData(title, array, correctAnswerIndex){
+//     let data = {};
+//     data.title = title;
+//     data.array = array;
+//     data.correctAnswerIndex = correctAnswerIndex;
+       
+//     data.isAnswerCorrect = function(index) {
+//        return index === data.correctAnswerIndex;
+//     }
+//     data.getCorrectAnswer = function() {
+//        return data.array[data.correctAnswerIndex];
+//     }
+//     return data;
+// }
+
+// let data = getData('Where is the capital of Jordan', ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'], 1);
+
+// - Convert the function to use `this` keyword
+
+function getData(title, array, correctAnswerIndex){
     let data = {};
     data.title = title;
     data.array = array;
     data.correctAnswerIndex = correctAnswerIndex;
        
     data.isAnswerCorrect = function(index) {
-       return `${title} is written by ${author} `;
+       return index === this.correctAnswerIndex;
     }
-    data.getCorrectAnswer = function(index) {
-       return `${title} is written by ${author} `;
+    data.getCorrectAnswer = function() {
+       return this.array[this.correctAnswerIndex];
     }
     return data;
 }
 
-let data = getData('Where is the capital of Jordan', ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'], 1);
-
-// - Convert the function to use `this` keyword
-
-function  getData(title, author){
-    let data = {};
-    this.title = title;
-    this.author = author;
-       
-    book.getSummary = function(author, title) {
-       book.author = author;
-       book.title = title;
-       return `${title} is written by ${author} `;
-    }
-    return book;
-}
-let data = getData("vineethepooh", "vineet");
 
 // - Write test by creating two objects also test both methods.
 
+let data = getData('Where is the capital of Jordan', ['Tashkent', 'Amaan', 'Kuwait City', 'Nairobi'], 2);
 
+let data2 = getData('where is the capital of india', ['Mumbai', 'Delhi', 'kolkatta', 'banglore'], 3);
