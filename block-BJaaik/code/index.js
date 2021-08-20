8bbc6fff3253c51665395dda0d2ef176a2491241
Vineet <vineet98love@gmail.com>
2021-08-20 12:56:28 +0530

    block-BJaaik

--- block-BJaaik/code/index.js
+++ block-BJaaik/code/index.js
@@ -5,51 +5,58 @@ class Person {
         this.gender = gender;
     }
     eat(){
-        console.log(`${name} is eating. and age is ${age}`); 
+        console.log(`${this.name} is eating. and age is ${this.age}`); 
     }
     sleep(){
-        console.log(`${name} is sleeping. and age is ${age}`);
+        console.log(`${this.name} is sleeping. and age is ${this.age}`);
     }
     walk(){
-        console.log(`${name} is walking. and age is ${age}`);
+        console.log(`${this.name} is walking. and age is ${this.age}`);
     }
 };
 class Player extends Person{
-    constructor(sportsName){
+    constructor(sportsName, name, age, gender){
         super(name,age,gender)
         this.sportsName = sportsName;
     }
     play(){
-        console.log(`${name} is playing. and age is ${age}`);  
+        console.log(`${this.name} is playing. and age is ${this.age}`);  
     }
 };
-class Cricketer extends Players{
-    constructor(teamName){
-        super(sportsName);
+class Cricketer extends Player{
+    constructor(teamName,name, age, gender){
+        super(sportsName, name, age, gender);
         this.teamName = teamName;
     }
     playCricket(){
+        console.log(`${this.name} is playing. and age is ${this.age}`); 
     }
 };
 
 
 class Teacher extends Person{
-    constructor(instituteName){
+    constructor(instituteName, name, age, gender){
         super(name,age,gender)
         this.instituteName = instituteName;
     }
     teach(){
-        console.log(`${name} is teaching. and age is ${age}. She ia ${gender}`); 
+        console.log(`${this.name} is teaching. and age is ${this.age}. She ia ${this.gender}`); 
     }
 };
 class Artist extends Person{
-    constructor(kind){
-        super(name,age,gender)
+    constructor(kind, name, age, gender){
+        super(name,age,gender);
+
         this.kind = kind;
     }
     createArt(){
-        console.log(`${name} is an artists. and age is ${age}. She ia ${gender}`);
+        console.log(`${this.name} is an artists. and is ${this.age}. She is ${this.gender}`);
     }
 };
+
+let person1 = new Person('vinee', 22, 'male');
+
+let artist1 = new Artist('kindHearted', 'patty', 34, 'female');
+
 
 
